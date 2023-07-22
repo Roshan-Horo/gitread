bef000c55dc61b32a0ad32a307d6809e32cd2460
LadyAudrey <theaudreygreene@gmail.com>
2023-07-22 14:25:20 -0700

    git sta

--- src/page/CodePage.jsx
+++ src/page/CodePage.jsx
@@ -1,11 +1,36 @@
-import { Header, Box, PageLayout } from "@primer/react";
+import { Header, Textarea, Box, PageLayout } from "@primer/react";
+
 import SplitContainer from "../components/SplitContainer";
 
 export default function CodePage({ param }) {
   return (
     <Box bg="canvas.default" width="100%" minHeight="100vh">
-      <PageLayout></PageLayout>
-      <SplitContainer param={param} />
+      <PageLayout>
+        <PageLayout.Header>
+          <Header
+            width="100vw"
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          ></Header>
+          <Header.Item>
+            <Textarea>Owner</Textarea>
+            <Textarea>RepoName</Textarea>
+            <Textarea>Branch</Textarea>
+          </Header.Item>
+          <Header.Item>
+            <Avatar
+              src="https://github.com/octocat.png"
+              size={80}
+              square
+              alt="@octocat"
+            />
+          </Header.Item>
+        </PageLayout.Header>
+        <SplitContainer param={param} />
+      </PageLayout>
     </Box>
   );
 }
